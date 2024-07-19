004492590fda4fb85b910fd40466c0353e643a73
BohdanTaran <bohdantaran.work@gmail.com>
2024-07-19 18:46:22 +0200

    add functionality to slider

--- src/widgets/Header/Topper/components/AdSlider/AdSlider.tsx
+++ src/widgets/Header/Topper/components/AdSlider/AdSlider.tsx
@@ -1,52 +1,37 @@
-import { useEffect, useState } from 'react';
-import { ArrowIcon } from '../../../../../shared/Icons/ArrowIcon';
-
-const ads = [
-  {
-    id: 0,
-    text: '1 Lorem ipsum dolor sit consectetur dolor sit amet elit Quod beatae eveniet totam nisi sapiente laborum, ducimus nostrum repudiandae quia aspernatur.',
-  },
-  {
-    id: 1,
-    text: '2 Lorem ipsum dolor sit amet consectetur dolor sit amet',
-  },
-  {
-    id: 2,
-    text: '3 Lorem ipsum dolor sit consectetur dolor sit amet elit Sed ipsam sint totam dolorum quaerat in?',
-  },
-  {
-    id: 3,
-    text: '4 Lorem ipsum dolor sit amet consectetur dolor sit amet adipisicing elit. Soluta?',
-  },
-];
+import { ArrowIcon } from '../../../../../shared/Icons/ArrowIcon'
+import { useAdvertisements } from './hooks/useAdvertisements'
+import { useSlider } from './hooks/useSlider'
 
 export const AdSlider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const swipeToNextInfo = () => {
-    setCurrentIndex((prev) => (prev + 1) % ads.length);
-  };
-
-  const swipeToPrevInfo = () => {
-    setCurrentIndex((prev) => (prev - 1 + ads.length) % ads.length);
-  };
-
-  useEffect(() => {
-    const interval = setInterval(swipeToNextInfo, 5000);
-
-    return () => clearInterval(interval);
-  }, [currentIndex]);
+  const { advertisements, isLoading } = useAdvertisements();
+  const { swipeToPrevInfo, swipeToNextInfo, currentIndex } = useSlider(
+    advertisements.length,
+  );
 
   return (
-    <div className="w-full flex justify-center text-xs text-center space-x-1 tablet:space-x-6 laptop:text-base">
+    <div className="w-full flex justify-center text-xs text-center space-x-4 tablet:space-x-6 laptop:text-base">
       <button onClick={swipeToPrevInfo}>
         <ArrowIcon color={'white'} styles={'w-6'} />
       </button>
       <div className="space-x-3">
-        <span>{ads[currentIndex].text.split(' ').slice(0, 9).join(' ')}</span>
-        <a href="#" className="text-primary-20">
-          Learn more
-        </a>
+        {!isLoading ? (
+          <>
+            <span>
+              {advertisements[currentIndex]?.text
+                .split(' ')
+                .slice(0, 9)
+                .join(' ')}
+            </span>
+            <a
+              href={advertisements[currentIndex]?.url}
+              className="text-primary-20"
+            >
+              Learn more
+            </a>
+          </>
+        ) : (
+          <span>Loading...</span>
+        )}
       </div>
       <button onClick={swipeToNextInfo}>
         <ArrowIcon color={'white'} styles={'w-6'} isRight={true} />
