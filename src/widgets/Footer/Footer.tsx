4ad83f63325381cf85512882eb6de11097fc1c1a
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-07-20 10:48:01 +0300

    example of using sprite

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -1,3 +1,21 @@
+import svgSprite from '../../shared/Icons/icons.svg';
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex justify-center items-center bg-primary-10 ">
+      Footer
+      {
+        <>
+          <svg className="fill-primary-70 w-[25px]">
+            <use href={`${svgSprite}#icon-rocket`} />
+          </svg>
+          <input
+            className="h-[40px] w-[200px] w-full text-sm border border-solid rounded-lg pl-10 outline-none"
+            type="text"
+            placeholder="Your Email"
+          />
+        </>
+      }
+    </div>
+  );
 };
