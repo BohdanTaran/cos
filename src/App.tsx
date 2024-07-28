a39eb121d5a4ce5db04cab0a3b64b75258dfe089
“Kateryna <yatskivkath@gmail.com>
2024-07-28 22:01:58 +0200

    context example

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BurgerProvider } from './contexts/HeaderContext'
+import { MediaQueryProvider } from './contexts/MediaQueryContext';
 import HomePage from './pages/HomePage/HomePage'
 import { Footer } from './widgets/Footer/Footer'
 import { Header } from './widgets/Header/Header'
@@ -6,9 +7,11 @@ import { Header } from './widgets/Header/Header'
 export const App = () => {
   return (
     <BurgerProvider>
+      <MediaQueryProvider>
       <Header />
       <HomePage />
       <Footer />
+      </MediaQueryProvider>
     </BurgerProvider>
   );
 };
--- src/contexts/MediaQueryContext.tsx
+++ src/contexts/MediaQueryContext.tsx
@@ -1,12 +1,28 @@
 import { useMediaQuery } from 'react-responsive';
 
-interface UseIsDeviceProps {
-  query: string;
-}
-
-const useIsDevice = ({ query }: UseIsDeviceProps) => {
-  const isDevice = useMediaQuery({ query });
-  return isDevice;
+const SIZES = {
+  mobile: `(max-width: 640px)`,
+  tablet: `(max-width: 1024px)`,
+  laptop: `(max-width: 1280px)`,
+  desktop: `(min-width: 1280px)`,
 };
 
-export default useIsDevice;
+
+export const MediaQueryContext = createContext<MediaQueryContextType | undefined>(undefined);
+
+interface MediaQueryProviderProps {
+  children: ReactNode;
+}
+
+export const MediaQueryProvider = ({ children }: MediaQueryProviderProps) => {
+  const isMobile = useMediaQuery({ query: SIZES.mobile });
+  const isTablet = useMediaQuery({ query: SIZES.tablet });
+  const isLaptop = useMediaQuery({ query: SIZES.laptop })
+
+  return (
+    <MediaQueryContext.Provider value={{ isMobile, isLaptop, isTablet }}>
+      {children}
+    </MediaQueryContext.Provider>
+  );
+};
+
