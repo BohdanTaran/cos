b8d87b04de251d402c71b20da0e540f7ecb0e71e
BohdanTaran <bohdantaran.work@gmail.com>
2024-08-03 18:23:01 +0200

    make as import

--- src/widgets/HomePageWidgets/Sections/BrandSection.tsx
+++ src/widgets/HomePageWidgets/Sections/BrandSection.tsx
@@ -1,3 +1,6 @@
+import FunkoImg from '../../../../public/funko.png';
+import MarvelImg from '../../../../public/marvel.png';
+import SWImg from '../../../../public/starwars.png';
 import Image from '../../../shared/components/Image';
 import ContentLayout from '../../../shared/layouts/ContentLayout';
 
@@ -5,15 +8,18 @@ const BrandSection = () => {
   return (
     <ContentLayout>
       <div className="h-[51px] justify-between flex items-center space-x-4 overflow-x-auto">
-        <Image src="../../../../public/marvel.png" styles="w-24" />
-        <Image src="../../../../public/funko.png" styles="w-24" />
-        <Image src="../../../../public/starwars.png" styles="w-24" />
-        <Image src="../../../../public/marvel.png" styles="w-24" />
-        <Image src="../../../../public/funko.png" styles="w-24" />
-        <Image src="../../../../public/starwars.png" styles="w-24" />
-        <Image src="../../../../public/marvel.png" styles="w-24" />
-        <Image src="../../../../public/funko.png" styles="w-24" />
-        <Image src="../../../../public/starwars.png" styles="w-24" />
+        <Image src={MarvelImg} styles="w-24" />
+        <Image src={FunkoImg} styles="w-24" />
+        <Image src={SWImg} styles="w-24" />
+        <Image src={MarvelImg} styles="w-24" />
+        <Image src={FunkoImg} styles="w-24" />
+        <Image src={SWImg} styles="w-24" />
+        <Image src={MarvelImg} styles="w-24" />
+        <Image src={FunkoImg} styles="w-24" />
+        <Image src={SWImg} styles="w-24" />
+        <Image src={MarvelImg} styles="w-24" />
+        <Image src={FunkoImg} styles="w-24" />
+        <Image src={SWImg} styles="w-24" />
       </div>
     </ContentLayout>
   );
