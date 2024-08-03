7640e2bc9e35108956c6698be6cb545d7c31b9ae
BohdanTaran <bohdantaran.work@gmail.com>
2024-08-03 17:09:34 +0200

    change place

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,13 @@
 import Carousel from '../../widgets/Carousel/Carousel';
-import BrandSection from '../../widgets/HomePageWidgets/Sections/BrandSection'
+import BrandSection from '../../widgets/HomePageWidgets/Sections/BrandSection';
 import SaleSection from '../../widgets/HomePageWidgets/Sections/SaleSection';
 import WhyShopSection from '../../widgets/HomePageWidgets/Sections/WhyShopSection';
 
 const HomePage = () => {
   return (
     <>
-      <Carousel title="Featured sets" />
       <BrandSection />
+      <Carousel title="Featured sets" />
       <SaleSection />
       <WhyShopSection />
     </>
