a13ec32934db290d2eeea696f5daae6fd6015366
BohdanTaran <bohdantaran.work@gmail.com>
2024-08-05 16:23:08 +0200

    configure router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import './globals.css';
+import HomePage from './pages/HomePage/HomePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/products:id',
+        element: <></>
+      }
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
