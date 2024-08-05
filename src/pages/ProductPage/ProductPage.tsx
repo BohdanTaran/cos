40b082e955518150b316ee03605c7998db91df56
BohdanTaran <bohdantaran.work@gmail.com>
2024-08-05 22:17:13 +0200

    add mock data

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -9,6 +9,20 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
 
+const data = {
+  title: 'Funko Pop action figure Harry Potter with a wand',
+  reviewMark: 4.5,
+  reviews: 3,
+  isInStock: true,
+  label: 'SALE',
+  price: 15,
+  oldPrice: 25,
+  discount: 40,
+  description:
+    'Introducing the classic plastic Harry Potter toy with his iconic wand! This detailed figure captures Harry in his Hogwarts uniform. Perfect for fans, this 6-inch collectible brings the Wizarding World magic to life.',
+  images: ['../../../public/FunkyPop.jpg', '../../../public/FunkyPop2.jpg'],
+};
+
 const ProductPage = () => {
   return (
     <>
@@ -23,15 +37,21 @@ const ProductPage = () => {
                 el: '.swiper-pagination',
               }}
             >
-              <SwiperSlide>
-                <Image src={FunkyPop} styles="" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <Image src={FunkyPop} styles="" />
-              </SwiperSlide>
+              {data.images.map((img) => {
+                return (
+                  <SwiperSlide className="flex justify-center">
+                    <Image src={img} styles="" />
+                  </SwiperSlide>
+                );
+              })}
             </Swiper>
             {/* Pagination of slider */}
             <div className="swiper-pagination static"></div>
+          </div>
+          <div className="py-10">
+            <h3 className="uppercase font-azeret font-bold text-xl">
+              {data.title}
+            </h3>
           </div>
         </div>
       </ContentLayout>
