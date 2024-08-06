dff6e1bd04aef77eb1d8e9fb4b55917c6a86ad49
BohdanTaran <bohdantaran.work@gmail.com>
2024-08-06 08:12:26 +0200

    add counter

--- src/pages/ProductPage/components/ProductActions.tsx
+++ src/pages/ProductPage/components/ProductActions.tsx
@@ -1,18 +1,34 @@
-import Icon from '../../../shared/components/Icon'
+import Icon from '../../../shared/components/Icon';
 
 const ProductActions = () => {
   return (
-    <div className="flex space-x-6 items-center pb-10">
-      <button className="px-10 w-full bg-primary-10 rounded-md min-h-[40px] max-h-[56px] text-sm">
-        Add to Cart
-      </button>
-      <Icon
-        icon="heart"
-        styles="w-9 h-9 cursor-pointer"
-        color="fill-transparent"
-        outlineColor="stroke-neutral-70"
-        outlineWidth={1.5}
-      />
+    <div className="flex flex-col items-center pb-10">
+      <div className="flex justify-center w-[154px] h-[48px] border border-neutral-10 rounded-md">
+        <button className="px-2 w-full">
+          <Icon icon="minus" color="fill-neutral-20" size="w-6 h-6" />
+        </button>
+        <input
+          className="w-[74px] text-center border-x border-neutral-10"
+          type="number"
+          readOnly={true}
+          value={1}
+        />
+        <button className="px-2 w-full">
+          <Icon icon="plus" color="fill-neutral-90" size="w-6 h-6" />
+        </button>
+      </div>
+      <div className="w-full flex space-x-6">
+        <button className="px-10 w-full bg-primary-10 rounded-md min-h-[40px] max-h-[56px] text-sm">
+          Add to Cart
+        </button>
+        <Icon
+          icon="heart"
+          styles="w-9 h-9 cursor-pointer"
+          color="fill-transparent"
+          outlineColor="stroke-neutral-70"
+          outlineWidth={1.5}
+        />
+      </div>
     </div>
   );
 };
