c8482a9cdc0a58a5ee46f0c422a72ededab78d41
BohdanTaran <bohdantaran.work@gmail.com>
2024-08-07 00:03:34 +0200

    responsive text

--- src/pages/ProductPage/components/ProductDetails.tsx
+++ src/pages/ProductPage/components/ProductDetails.tsx
@@ -5,7 +5,7 @@ import StarsReview from './StarsReview';
 const ProductDetails = ({ product }: IProductData) => {
   return (
     <div className="flex flex-col space-y-4 pt-10 pb-4 font-roboto">
-      <h3 className="uppercase font-azeret font-bold text-xl">
+      <h3 className="uppercase font-azeret font-bold text-xl laptop:text-3xl">
         {product.title}
       </h3>
       <div className="flex flex-col laptop:flex-row">
@@ -16,15 +16,17 @@ const ProductDetails = ({ product }: IProductData) => {
             ({product.reviews} {product.reviews > 1 ? 'reviews' : 'review'})
           </span>
         </div>
-        <span className="font-medium text-success-40 laptop:pl-6">In stock</span>
+        <span className="font-medium text-success-40 laptop:pl-6">
+          In stock
+        </span>
       </div>
 
       <ProductLabel label={product.label} direction="row" />
       <div className="flex w-9/12 items-center">
         <div className="w-[173px] mr-4">
-          <div className="font-bold text-xl">
+          <div className="font-bold text-xl laptop:text-3xl">
             <span className="mr-3 text-neutral-90">${product.price}</span>
-            <span className="text-neutral-20">${product.oldPrice}</span>
+            <span className="text-neutral-20 line-through">${product.oldPrice}</span>
           </div>
         </div>
         <span className="flex items-center justify-center min-h-[36px] min-w-[69px] bg-danger-10 rounded-[62px] text-danger-50 font-medium">
