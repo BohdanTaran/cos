704fb4c2088955a462b293a6e299a56b62ef8288
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-08-19 15:00:02 +0300

    Revert "Tcio 99/hero"

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,4 @@
 import Carousel from '../../widgets/Carousel/Carousel';
-import { Hero } from '../../widgets/HomePageWidgets/Hero/Hero';
 import BrandSection from '../../widgets/HomePageWidgets/Sections/BrandSection';
 import { ProductSection } from '../../widgets/HomePageWidgets/Sections/ProductSection';
 import SaleSection from '../../widgets/HomePageWidgets/Sections/SaleSection';
@@ -8,7 +7,6 @@ import WhyShopSection from '../../widgets/HomePageWidgets/Sections/WhyShopSectio
 const HomePage = () => {
   return (
     <>
-      <Hero />
       <BrandSection />
       <Carousel title="Featured sets" />
       <SaleSection />
