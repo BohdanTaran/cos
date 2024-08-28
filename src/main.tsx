edca91b69afe594d089d5572deab65c8eb484836
Bohdan Taran <bohdantaran.work@gmail.com>
2024-08-28 22:03:39 +0200

    [ant-less-config] add less instead of scss

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { App } from './App';
 import HomePage from './pages/HomePage/HomePage';
 import ProductPage from './pages/ProductPage/ProductPage';
 
-import '../scss/main.scss';
+import '../less/main.less';
 
 // TODO: remove it
 import './globals.scss';
