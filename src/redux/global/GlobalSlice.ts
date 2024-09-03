e1ed6150ec15638f9467e3d8e75da161246dd04b
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-09-03 10:13:02 +0300

    add redux

--- src/redux/global/GlobalSlice.ts
+++ src/redux/global/GlobalSlice.ts
@@ -1,1 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
 
+const globalSlice = createSlice({
+  name: 'global',
+  initialState: {
+    isLoading: false,
+    error: null,
+  },
+  reducers: {},
+});
+export const globalSliceReducer = globalSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,33 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import additionalMiddleware from 'additional-middleware';
-// import logger from 'redux-logger';
-// // @ts-ignore
-// import untypedMiddleware from 'untyped-middleware';
-// import rootReducer from './rootReducer';
-
-// export type RootState = ReturnType<typeof rootReducer>;
-// const store = configureStore({
-//   reducer: rootReducer,
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware()
-//       .prepend(
-//         // correctly typed middlewares can just be used
-//         additionalMiddleware,
-//         // you can also type middlewares manually
-//         untypedMiddleware as Middleware<
-//           (action: Action<'specialAction'>) => number,
-//           RootState
-//         >,
-//       )
-//       // prepend and concat calls can be chained
-//       .concat(logger),
-// });
-
-// export type AppDispatch = typeof store.dispatch;
-
-// export default store;
-// reducer: rootReducer,
-
 import {
   persistStore,
   persistReducer,
@@ -40,7 +11,7 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-import { globalReducer } from './global/GlobalSlice.js';
+import { globalSliceReducer } from './global/GlobalSlice.ts';
 
 const persistGlobalConfig = {
   key: 'global',
@@ -48,7 +19,7 @@ const persistGlobalConfig = {
   whitelist: [],
 };
 
-const persistedGlobal = persistReducer(persistGlobalConfig, globalReducer);
+const persistedGlobal = persistReducer(persistGlobalConfig, globalSliceReducer);
 
 const store = configureStore({
   reducer: {
@@ -63,5 +34,5 @@ const store = configureStore({
 });
 
 export const persistedStore = persistStore(store);
-export type RootState = ReturnType<typeof store.getState>
+
 export default store;
